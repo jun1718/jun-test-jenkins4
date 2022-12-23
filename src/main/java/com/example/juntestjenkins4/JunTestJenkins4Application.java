bc81f99e054bb318918772b0ff3921e88befef63
jun1718 <gbeovhsqhtka@naver.com>
2022-12-23 10:42:55 +0900

    Initial commit

---
+++ src/main/java/com/example/juntestjenkins4/JunTestJenkins4Application.java
@@ -0,0 +1,13 @@
+package com.example.juntestjenkins4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JunTestJenkins4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JunTestJenkins4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/juntestjenkins4/JunTestJenkins4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.juntestjenkins4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JunTestJenkins4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
